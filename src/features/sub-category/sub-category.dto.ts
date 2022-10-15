f2cae863c2c21ec38144078e9e773ef2044fed04
salam-grype <salam@grype.cagit config --global user.email salam@grype.cagit config --global user.email salam@grype.ca>
2022-10-15 23:46:52 +0600

    added partial

--- src/features/sub-category/sub-category.dto.ts
+++ src/features/sub-category/sub-category.dto.ts
@@ -1,4 +1,5 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { SubCategory } from './sub-category.model';
 
 export class CreateSubCategoryDto extends SubCategory {}
-export class UpdateSubCategoryDto extends CreateSubCategoryDto {}
+export class UpdateSubCategoryDto extends PartialType(CreateSubCategoryDto) {}
