b808e05f3df4fde2ee632db18e1a5b2f5830886a
salam-grype <salam@grype.cagit config --global user.email salam@grype.cagit config --global user.email salam@grype.ca>
2022-10-15 18:24:36 +0600

    updated category

--- src/features/category/category.model.ts
+++ src/features/category/category.model.ts
@@ -1,12 +1,15 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { IsString } from 'class-validator';
 
 export type CategoryDocument = Category & Document;
 
 @Schema({ timestamps: true })
 export class Category {
+  @IsString()
   @Prop({ type: String, required: true })
   name: string;
 
+  @IsString()
   @Prop({ type: String, required: true, unique: true })
   permalink: string;
 }
