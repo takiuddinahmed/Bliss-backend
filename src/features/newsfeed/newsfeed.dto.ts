7c2e3e007f3431d456e02e8b6be0dd8acbc8208c
kmasalam <mccyber007@gmail.com>
2023-02-12 16:08:40 +0600

    newsfeed update

--- src/features/newsfeed/newsfeed.dto.ts
+++ src/features/newsfeed/newsfeed.dto.ts
@@ -1,5 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { FileData } from '../common';
 import { Newsfeed } from './newsfeed.model';
 
 export class CreateNewsfeedDto extends Newsfeed {}
 export class UpdateNewsfeedDto extends PartialType(CreateNewsfeedDto) {}
+export type NewUpdateNewstDto = UpdateNewsfeedDto & {
+  oldAttachment?: FileData[];
+};
