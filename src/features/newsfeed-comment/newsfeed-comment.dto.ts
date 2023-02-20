19f2afb85e43f653b6e6c3ba05e6323c60b53730
Takiuddin Ahmed <takiuddinahmed@gmail.com>
2023-02-20 21:37:44 +0600

    fix

--- src/features/newsfeed-comment/newsfeed-comment.dto.ts
+++ src/features/newsfeed-comment/newsfeed-comment.dto.ts
@@ -1,4 +1,7 @@
+import { PartialType } from '@nestjs/mapped-types';
 import { NewsfeedComment } from './newsfeed-comment.model';
 
 export class CreateNewsfeedCommentDto extends NewsfeedComment {}
-export class UpdateNewsfeedCommentDto extends CreateNewsfeedCommentDto {}
+export class UpdateNewsfeedCommentDto extends PartialType(
+  CreateNewsfeedCommentDto,
+) {}
